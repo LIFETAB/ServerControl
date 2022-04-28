9e7600af781f14f230e733800e666a7c2782f055
LIFETAB <43006406+LIFETAB@users.noreply.github.com>
2022-04-28 21:25:36 +0200

    1.0.0

--- src/main/java/one/lifetab/mc/servercontrol/commands/Commands.java
+++ src/main/java/one/lifetab/mc/servercontrol/commands/Commands.java
@@ -1,33 +1,51 @@
 package one.lifetab.mc.servercontrol.commands;
 
-import one.lifetab.mc.servercontrol.Servercontrol;
 import one.lifetab.mc.servercontrol.utility.Messages;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.concurrent.TimeUnit;
+
 import static one.lifetab.mc.servercontrol.Servercontrol.PREFIX;
+import static one.lifetab.mc.servercontrol.utility.Send.broadcast;
 import static one.lifetab.mc.servercontrol.utility.Send.msg;
 
 public class Commands {
     public static void help(@NotNull CommandSender sender) {
-        if (sender.hasPermission("servercontrol.help")) {
+        if (sender.hasPermission("servercontrol.help") || sender instanceof ConsoleCommandSender) {
             Messages.helpMsg(sender);
         }
     }
 
     public static void version(@NotNull CommandSender sender) {
-        if (sender.hasPermission("servercontrol.version")) {
+        if (sender.hasPermission("servercontrol.version") || sender instanceof ConsoleCommandSender) {
             Messages.versionMsg(sender);
         }
     }
 
     public static void reload(@NotNull CommandSender sender) {
-        msg(sender, PREFIX + "&cCommand in development...");
+        if (sender.hasPermission("servercontrol.reload") || sender instanceof ConsoleCommandSender) {
+            msg(sender, PREFIX + "&cCommand in development...");
+        }
     }
 
     public static void restart(@NotNull CommandSender sender) {
-        if (sender.hasPermission("servercontrol.restart")) {
+        if (sender.hasPermission("servercontrol.restart") || sender instanceof ConsoleCommandSender) {
+            for (int i = 10; i > 0; i--) {
+                broadcast(PREFIX + "&cServer restarting in &e" + i + "&c seconds...");
+                try {
+                    TimeUnit.SECONDS.sleep(1); // Sleep for 1 second
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            for (Player target : Bukkit.getOnlinePlayers()) {
+                target.kickPlayer(ChatColor.translateAlternateColorCodes('&',PREFIX + "&cServer restarting... \n&cPlease reconnect in 2 minutes..."));
+            }
             Bukkit.spigot().restart();
         }
     }
--- src/main/java/one/lifetab/mc/servercontrol/utility/Send.java
+++ src/main/java/one/lifetab/mc/servercontrol/utility/Send.java
@@ -1,11 +1,18 @@
 package one.lifetab.mc.servercontrol.utility;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class Send {
     public static void msg(@NotNull CommandSender player, @NotNull String message) {
         player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+    }
+    public static void broadcast(@NotNull String message) {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+        }
     }
 }
